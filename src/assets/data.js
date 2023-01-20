25fcd2218b3b88ba796a8396f010f1cc4b687969
saumyanayak <saumyanayak264@gmail.com>
2023-01-21 04:59:05 +0530

    commit

---
+++ src/assets/data.js
@@ -0,0 +1,36 @@
+const data = [
+    {
+        roomName: "Guest Room",
+        roomLink : "http://192.168.1.7/guestroom",
+    },
+    {
+        roomName: "Lounge Room",
+        roomLink : "http://192.168.1.7/swimmingpool",
+    },
+    {
+        roomName: "Lift Room",
+        roomLink : "http://192.168.1.7/liftroom",
+    },
+    {
+        roomName: "Lounge Room",
+        roomLink : "http://192.168.1.7/loungeroom",
+    },
+    {
+        roomName: "Bed Room",
+        roomLink : "http://192.168.1.7/bedroom",
+    },
+    {
+        roomName: "Study Room",
+        roomLink : "http://192.168.1.7/studyroom",
+    },
+    {
+        roomName: "Fan",
+        roomLink : "http://192.168.1.7/fan",
+    }
+    // ,{
+    //     roomName :"Buzzer",
+    //     roomLink : "192.168.1.7/buzzer"
+    // }
+]
+
+export default data
