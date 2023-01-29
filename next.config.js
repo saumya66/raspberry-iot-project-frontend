c272f4f199e27ac82bc3d658217774326d717697
saumyanayak <saumyanayak264@gmail.com>
2023-01-30 00:30:17 +0530

    new way

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'https://api.example.com/:path*',
+        },
+      ]
+    },
+};
